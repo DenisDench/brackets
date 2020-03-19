d9945142a82f53d7f41aca95dadeb72e846972de
Denis Salikhov <salihov.den@mail.ru>
2020-03-20 02:26:35 +0700

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  do {
+    var strBefore = str;
+    for (let [left, right] of bracketsConfig) {
+        str = str.replace(`${left}${right}`, '');
+    }
+  } while (str !== strBefore)
+
+  return !str;
 }
